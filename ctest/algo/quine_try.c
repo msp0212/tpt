1d001bd1ff43f9d05196238a00a0496b2414877e
msp0212 <mohitbravo007@gmail.com>
2019-07-05 12:45:15 +0530

    quine

--- ctest/algo/quine_try.c
+++ ctest/algo/quine_try.c
@@ -1,15 +1,3 @@
 #include <stdio.h>
-#include <stdint.h>
-
-int main()
-{
-	char *s = "#include<stdio.h>\n\n"
-		"int main()\n"
-		"{\n"
-		"char *s = \"%s\"\n"
-		"printf(s,s)\n"
-		"return 0;\n"
-		"}\n";
-	printf(s,s);
-	return 0;
-}
+char*s="#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
+int main(void){printf(s,10,34,s,34,10,10);}
